96bedd92429fedd7e1ecebdacf29ddf2f9d5f246
marceloaaps <marceloalexandreaps@gmail.com>
2023-12-26 11:04:55 -0300

    Criação do package Chess, onde estará realmente o jogo e diversas configurações para iniciar o tabuleiro.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -29,6 +29,14 @@ public class Board {
         this.columns = columns;
     }
 
+    public Piece piece (int row, int columns){
+        return pieces[row][columns];
+    }
+
+    public Piece piece (Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 
 
 }
