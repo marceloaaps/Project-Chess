c163ad3f4a60f2fcba2881b12cf52dcebb8c97df
marceloaaps <marceloalexandreaps@gmail.com>
2023-12-26 11:55:39 -0300

    Acresentadas as classes de algumas peças e alterações no tabuleiro e partida.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -37,6 +37,11 @@ public class Board {
         return pieces[position.getRow()][position.getColumn()];
     }
 
+    public void PlacePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
 
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import pieces.King;
+import pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,16 +11,23 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-        for (int i = 0; i > board.getRows(); i++) {
-            for (int j = 0; j > board.getColumns(); j++) {
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
+    }
+
+    public void initialSetup(){
+        board.PlacePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.PlacePiece(new King(board, Color.BLACK), new Position (0, 4));
+        board.PlacePiece(new King(board, Color.WHITE), new Position (7, 4));
     }
 
 }
