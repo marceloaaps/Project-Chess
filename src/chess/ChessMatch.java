0443f5f4317d04e0c7d0772cd0cadb640c5a17fe
marceloaaps <marceloalexandreaps@gmail.com>
2023-12-26 16:50:55 -0300

    Adicionadas novas Exceptions para possíveis missconcepts dos usuários e atualização na introdução das peças ao tabuleiro.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,10 +24,14 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.PlacePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     public void initialSetup(){
-        board.PlacePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.PlacePiece(new King(board, Color.BLACK), new Position (0, 4));
-        board.PlacePiece(new King(board, Color.WHITE), new Position (7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 
 }
